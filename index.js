5c130652d9a47f4be757b04c0f77e9643e5c2727
Braily Guzman <brailyg@outlook.com>
2024-10-02 14:55:03 -0400

    implemented multiplesOfThreeOrFive function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 //Code your solutions in this file
+
+// Assignment 1
 const fiveToOneHundred = () => {
     for (let i = 5; i <= 100; i++) {
         console.log(i);
@@ -10,9 +12,20 @@ const multiplesOfThree = () => {
     for (let i = 3; i <= 100; i += 3) {
         console.log(i);
     }
-    
+};
+
+// Assignment 3
+const multiplesOfThreeOrFive = () => {
+    for (let i = 0; i <= 100; i++) {
+        if (i % 3 === 0) {
+            console.log(i);
+        } else if (i % 5 === 0) {
+            console.log(i);
+        }
+    }
 };
 
 // Tests
 // fiveToOneHundred();
 // multiplesOfThree();
+// multiplesOfThreeOrFive();
